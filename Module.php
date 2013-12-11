5b12ecd82fdbd245d37700def139740cc0cb8dad
Wayne <kwamaking@gmail.com>
2013-12-11 13:18:46 -0600

    changing composer and rearranged tests.

--- Module.php
+++ Module.php
@@ -24,14 +24,4 @@ class Module
             )
         );
     }
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                )
-            )
-        );
-    }
 }
